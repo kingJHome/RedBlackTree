f188d5bbc9d68be5531e57497cc2e81b3831f3ba
caojin <951086498@qq.com>
2019-01-10 11:02:32 +0800

    add header hander file

---
+++ treeHander.c
@@ -0,0 +1,12 @@
+#include "treeHander.h"
+
+linkTree CreateNode(int data){
+}
+
+//插入节点
+linkTree InsertNode(linkTree anew,linkTree root){
+}
+
+//删除节点
+linkTree DelNode(linkTree root,int sdata){
+}
---
+++ treeHander.h
@@ -0,0 +1,24 @@
+#ifndef _RED_BLACK_TREE_H
+#define _RED_BLACK_TREE_H
+
+typedef enum MyColor{ red, black } MyColor;
+
+typedef struct _tree_node{
+	int data;
+	struct _tree_node *lchild;
+	struct _tree_node *rchild;
+	struct _tree_node *parent;
+	struct _tree_node *uncle;
+	MyColor color;
+}treenode, *linktree;
+
+//创建节点
+linkTree CreateNode(int data);
+
+//插入节点
+linkTree InsertNode(linkTree anew,linkTree root);
+
+//删除节点
+linkTree DelNode(linkTree root,int sdata);
+
+#endif
